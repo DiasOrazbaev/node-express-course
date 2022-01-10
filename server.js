18cac69ed768677bc82ba1cce0b2d3409851d748
Dias <orazbaevdev@gmail.com>
2022-01-10 17:32:04 +0600

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+const mookUserData = [
+    {name: "Mark"},
+    {name: "Jame"}
+]
+
+app.get('/users', function (req,res) {
+    res.json({
+        success: true,
+        message: 'Successful got users',
+        users: mookUserData
+    })
+})
+
 app.listen(8000, function () {
     console.log(`server is running`)
 })
